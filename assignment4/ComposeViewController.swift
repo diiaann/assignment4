83333edd5c4118a4d9bde719fc28819b6f8c8329
Dian <diiaann@gmail.com>
2016-02-28 09:13:20 -0800

    most required stories

---
+++ assignment4/ComposeViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ComposeViewController.swift
+//  assignment4
+//
+//  Created by Diandian Xiao on 2/26/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    
+    @IBOutlet var icons: [UIImageView]!
+
+
+    //TODO: nevermind
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        for icon in icons {
+            
+            UIView.animateWithDuration(0.6, delay: NSTimeInterval(drand48()), options: [], animations: { () -> Void in
+                icon.center = CGPoint (x: icon.center.x, y: icon.center.y - 480)
+                }, completion: { (Bool) -> Void in
+            })
+        }
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // do stuff
+        
+        for icon in icons {
+            icon.center = CGPoint (x: icon.center.x, y: icon.center.y + 480)
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ assignment4/TabViewController.swift
@@ -0,0 +1,90 @@
+//
+//  TabViewController.swift
+//  assignment4
+//
+//  Created by Diandian Xiao on 2/26/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    var homeViewController: UIViewController!
+    var searchViewController: UIViewController!
+    var accountViewController: UIViewController!
+    var trendingViewController: UIViewController!
+    var viewControllers: [UIViewController]!
+    
+    var fadeTransition: FadeTransition!
+    
+    var selectedIndex: Int = 0
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        homeViewController = storyboard.instantiateViewControllerWithIdentifier("HomeViewController")
+        searchViewController = storyboard.instantiateViewControllerWithIdentifier("SearchViewController")
+        accountViewController = storyboard.instantiateViewControllerWithIdentifier("AccountViewController")
+        trendingViewController = storyboard.instantiateViewControllerWithIdentifier("TrendingViewController")
+
+        viewControllers = [homeViewController, searchViewController, accountViewController, trendingViewController]
+        
+        buttons[selectedIndex].selected = true
+        didPressTab(buttons[selectedIndex])
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func didPressTab(sender: UIButton) {
+        //store the old index and set the new one
+        let previousIndex = selectedIndex
+         selectedIndex = sender.tag
+        
+        //unselect old button, select new one
+        buttons[previousIndex].selected = false
+        sender.selected = true
+
+        let previousVC = viewControllers[previousIndex]
+        let vc = viewControllers[selectedIndex]
+        
+        previousVC.willMoveToParentViewController(nil)
+        previousVC.view.removeFromSuperview()
+        previousVC.removeFromParentViewController()
+        
+        addChildViewController(vc)
+
+        vc.view.frame = contentView.bounds
+        contentView.addSubview(vc.view)
+        
+        vc.didMoveToParentViewController(self)
+        
+    }
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        // Access the ViewController that you will be transitioning too, a.k.a, the destinationViewController.
+        var destinationViewController = segue.destinationViewController
+        
+        // Set the modal presentation style of your destinationViewController to be custom.
+        destinationViewController.modalPresentationStyle = UIModalPresentationStyle.Custom
+        
+        // Create a new instance of your fadeTransition.
+        fadeTransition = FadeTransition()
+        
+        // Tell the destinationViewController's  transitioning delegate to look in fadeTransition for transition instructions.
+        destinationViewController.transitioningDelegate = fadeTransition
+        
+        // Adjust the transition duration. (seconds)
+        fadeTransition.duration = 1.0
+    }
+
+}
